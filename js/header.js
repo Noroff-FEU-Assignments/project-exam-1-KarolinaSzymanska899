8e160fcebf018239263b2647770ab2f8eaaee73f
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-02 22:35:23 +0200

    styling

--- js/header.js
+++ js/header.js
@@ -1,1 +1,10 @@
-export function createHeader() {}
+export function createHeader() {
+  const header = document.querySelector("#header");
+
+  header.innerHTML = `<a href="../posts.html">BLOG</a>
+                      <a href="../posts.html">BAKERY</a>
+                      <a href="../posts.html">DESSERTS</a>
+                      <a href="../posts.html">PASTA</a>
+                      <a href="../posts.html">SALADS</a>
+                      <a href="../posts.html">SOUPS</a>`;
+}
