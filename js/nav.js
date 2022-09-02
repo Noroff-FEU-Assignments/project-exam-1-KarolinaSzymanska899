4fb07bb9a767d91ccd32005410f60e9e4d0ec80e
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-02 12:03:25 +0200

    making a slideout nav

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,18 @@
 export function createNav() {
   const nav = document.querySelector("#nav");
 
-  nav.innerHTML = `<input type="checkbox" id="toggle" />
-                  <label for="toggle"><img src="../images/cheeseburger.png" /></label>
-                  <ul id="navbar-nav" class="navbar-nav">
+  nav.innerHTML = `<img id="toggle-btn" class="toggle-btn" src="../images/cheeseburger.png" />
+                  <ul>
                     <li class="nav-item"><a href="index.html">HOME</a></li>
                     <li class="nav-item"><a href="posts.html">RECIPES</a></li>
                     <li class="nav-item"><a href="#">BLOG</a></li>
                     <li class="nav-item"><a href="about.html">ABOUT</a></li>
                     <li class="nav-item"><a href="contact.html">COLLABORATION</a></li>
                   </ul>`;
+}
+
+export function toggleNav() {
+  const toggleBtn = document.querySelector("#toggle-btn");
+  nav.classList.toggle("active");
+  toggleBtn.addEventListener("onclick", toggleNav);
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import { createNav } from "./nav.js";
+import { toggleNav } from "./nav.js";
 import { createHeader } from "./header.js";
 import { createFooter } from "./footer.js";
 
 createNav();
+toggleNav();
 createHeader();
 createFooter();
