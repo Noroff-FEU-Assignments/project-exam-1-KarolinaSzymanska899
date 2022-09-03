78e509f38e8465727bb36a45e444d6927b1d21ae
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-03 13:15:07 +0200

    added imported function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 import { createNav } from "./nav.js";
 import { createHeader } from "./header.js";
+import { createSidebar } from "./sidebar.js";
 import { createFooter } from "./footer.js";
 import { toggleNav } from "./nav.js";
 
 createNav();
 createHeader();
+createSidebar();
 createFooter();
