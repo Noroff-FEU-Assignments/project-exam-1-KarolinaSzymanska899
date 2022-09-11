44348f5fefd8e406c7082f61d4bada0e75f11c4a
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-12 00:36:11 +0200

    uncommented

--- js/ui/carousel.js
+++
@@ -1,7 +0,0 @@
-/*
-<img class="post-img" src="${post._embedded["wp:featuredmedia"][0].source_url}" alt="${post.title.rendered}" />
-*/
-
-/*
-https://karolinaszymanska.tech/heidicooks/wp-json/wp/v2/posts?_embed
-*/
