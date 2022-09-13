25b73a0ec03c60e81e6512dec5b83bbdd1832e8c
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-13 23:30:58 +0200

    added font awesome scripts

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,8 @@
 export function createFooter() {
   const footer = document.querySelector("#footer");
 
-  footer.innerHTML = `<div class="footer-text">
+  footer.innerHTML = `<div class="footer-arrow"><a href="#header" rel="noopener" aria-label="to the top of the visit page"><i class="fa-regular fa-circle-up"></i></a></div>
+                      <div class="footer-text">
                         <p>© 2007-2022 Heidi Cooks</p>
                         <p>All rights reserved</p>
                         <p>Copying and distribution without the consent of heidicooks.com is prohibited</p>
