14ab1ad8591da6f454f94aad145816e005ab2751
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-09-13 17:34:38 +0200

    trying to make function searchPosts work

--- js/ui/searchPosts.js
+++ js/ui/searchPosts.js
@@ -1,19 +1,38 @@
-import { renderTeams } from "./renderTeams.js";
+import { renderPosts } from "./renderPosts";
 
-export function searchTeams(teams) {
-  const search = document.querySelector(".search");
+export function searchPosts(posts) {
+  const searchButton = document.querySelector("#search-button");
+  const searchInput = document.querySelector("#search-input");
 
-  search.onkeyup = function (event) {
-    // console.log(event);
+  searchButton.onclick = function () {
+    console.log("it works");
+    const searchValue = searchInput.value.trim().toLowerCase();
 
-    const searchValue = event.target.value.trim().toLowerCase();
+    const newUrl = baseUrl + `?search=${searchValue}`;
 
+    postsContainer.innerHTML = "";
+    renderPosts(newUrl);
+  };
+}
+/*
     const filteredTeams = teams.filter(function (team) {
       if (team.full_name.toLowerCase().startsWith(searchValue)) {
         return true;
       }
     });
 
-    renderTeams(filteredTeams);
-  };
-}
+    renderPosts(filteredTeams);
+
+
+
+    // to dodalam:
+    const searchButton = document.querySelector("#search-button");
+
+    searchButton.onclick = function() {
+      const searchInput = document.querySelector("#search-input").value;
+      const newUrl = baseUrl + `?search=${searchInput}`;
+
+      postsContainer.innerHTML = "";
+      renderPosts(newUrl);
+    }
+    */
